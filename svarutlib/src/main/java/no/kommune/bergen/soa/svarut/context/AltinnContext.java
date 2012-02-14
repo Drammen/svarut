c317c419fbdcc3088dbf12e92cdec5250d460bb1
Idar Borlaug <idar.borlaug@bouvet.no>
2012-02-14 09:47:45 +0100

    Fjernet xml config for AltinnContext

--- svarutlib/src/main/java/no/kommune/bergen/soa/svarut/context/AltinnContext.java
+++ svarutlib/src/main/java/no/kommune/bergen/soa/svarut/context/AltinnContext.java
@@ -2,15 +2,25 @@ package no.kommune.bergen.soa.svarut.context;
 
 import no.kommune.bergen.soa.svarut.altin.CorrespondenceSettings;
 import no.kommune.bergen.soa.svarut.util.DispatchWindow;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * Benyttes som bærer av kofigurasjon for Altinn distribusjoner. Obs: LeadTime betyr her antall dager mottaker har på å lese et
  * dokument før dokumentet må sendes i posten
  */
+@Component("altinnContext")
 public class AltinnContext {
+
+	@Autowired
 	private CorrespondenceSettings correspondenceSettings;
-	private long leadTimeApost = 2, leadTimeBpost = 2, leadTimeRekommandert = 2;
-    private DispatchWindow dispatchWindow;
+
+	private long leadTimeApost = 2, leadTimeBpost = 2, leadTimeRekommandert = 1;
+
+	@Resource(name = "altinnDispatchWindow")
+	private DispatchWindow dispatchWindow;
 
 	public void verify() {
 		if (correspondenceSettings == null) throw new RuntimeException( "Undefined field: correspondenceSettings" );
