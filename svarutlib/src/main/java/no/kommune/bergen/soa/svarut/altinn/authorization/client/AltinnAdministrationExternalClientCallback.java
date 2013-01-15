70af771c365b2ac01734fd0b45680b7615ed4f6f
Jarle <jarle.borsheim@bouvet.no>
2013-01-15 12:44:32 +0100

    PIA-1573 Bruker Constrettor til a hente passord i AltinnAdministrationExternalClientCallback.

--- svarutlib/src/main/java/no/kommune/bergen/soa/svarut/altinn/authorization/client/AltinnAdministrationExternalClientCallback.java
+++ svarutlib/src/main/java/no/kommune/bergen/soa/svarut/altinn/authorization/client/AltinnAdministrationExternalClientCallback.java
@@ -7,12 +7,22 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
 import org.apache.ws.security.WSPasswordCallback;
+import org.constretto.ConstrettoBuilder;
+import org.constretto.ConstrettoConfiguration;
+import org.springframework.core.io.DefaultResourceLoader;
 
 public class AltinnAdministrationExternalClientCallback implements CallbackHandler {
 
+	String password;
+
+	public AltinnAdministrationExternalClientCallback() {
+		ConstrettoConfiguration config = new ConstrettoBuilder().createPropertiesStore().addResource(new DefaultResourceLoader().getResource("classpath:application.properties")).done().getConfiguration();
+		password = config.evaluateToString("altinn.systemPassword");
+	}
+
 	@Override
 	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-	    WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
-	    pc.setPassword("UO6pXuIt");
+		WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+		pc.setPassword(password);
 	}
 }
