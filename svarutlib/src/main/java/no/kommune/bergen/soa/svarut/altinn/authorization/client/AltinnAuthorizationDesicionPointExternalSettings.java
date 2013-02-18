19d9c3df73149579f7970f11bd74fe5fcbb9fb66
Jarle <jarle.borsheim@bouvet.no>
2013-02-18 09:00:55 +0100

    PIA-1573 lagt til service edition constretto tag.
    Conflicts:
    	svarutlib/src/main/java/no/kommune/bergen/soa/svarut/altinn/authorization/client/AltinnAuthorizationDesicionPointExternalSettings.java
    	svarutlib/src/test/resources/application.properties

---
+++ svarutlib/src/main/java/no/kommune/bergen/soa/svarut/altinn/authorization/client/AltinnAuthorizationDesicionPointExternalSettings.java
@@ -0,0 +1,46 @@
+package no.kommune.bergen.soa.svarut.altinn.authorization.client;
+
+import no.kommune.bergen.soa.svarut.altinn.AltinnCommonSettings;
+import no.kommune.bergen.soa.svarut.altinn.AltinnServiceSettings;
+
+import org.constretto.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AltinnAuthorizationDesicionPointExternalSettings extends AltinnCommonSettings implements AltinnServiceSettings {
+
+	@Configuration(expression = "altinn.authorization.endpoint")
+	String endpoint;
+	@Configuration(expression = "altinn.authorization.service.edition")
+	String serviceEdition;
+	@Configuration(expression = "altinn.authorization.environment")
+	String environment;
+
+	@Override
+	public String getEndpoint() {
+		return endpoint;
+	}
+
+	@Override
+	public void setEndpoint(String endpoint) {
+		this.endpoint = endpoint;
+	}
+
+	@Override
+	public String getServiceEdition() {
+		return serviceEdition;
+	}
+
+	@Override
+	public void setServiceEdition(String serviceEdition) {
+		this.serviceEdition = serviceEdition;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+}
